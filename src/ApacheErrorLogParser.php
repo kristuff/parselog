cd537f4bc25b0d09074a2350e53a7a74f6e1dfea
kristuff <kristuff.fr@outlook.com>
2020-09-26 12:11:12 +0200

    Update ApacheErrorLogParser.php

--- src/ApacheErrorLogParser.php
+++ src/ApacheErrorLogParser.php
@@ -32,17 +32,17 @@ class ApacheErrorLogParser extends LogParser
      * 
      * @var string
      */
-    public $defaultFormat = '%t %l ?(%P )?(%a )%M';
+    public $defaultFormat = '%t %l ?(%P )??(%a )?%M';
 
     /** 
      * @var array 
      */
     public $patterns = [
         '%%' => '(?P<percent>\%)',
-        '%a' => '?(\[client (?P<remoteIp>{{PATTERN_IP_ALL}}):[\d]+\])',
+        '%a' => '\[client (?P<remoteIp>{{PATTERN_IP_ALL}}):[\d]+\]',
         '%A' => '(?P<localIp>{{PATTERN_IP_ALL}})',
         '%l' => '\[(?P<severity>[\w:]+)\]',
-        '%P' => '?(\[pid (?P<pid>\d+)\])',
+        '%P' => '\[pid (?P<pid>\d+)\]',
         '%t' => '\[(?P<time>(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun) (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{2} \d{2}:\d{2}:\d{2}\.\d{6} \d{4})\]',
         '%M' => '(?P<message>.+?)',
      ]; 
